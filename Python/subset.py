6b5f1f7b9cc88191174c3f39897478f15bf675d3
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-03-15 21:43:35 -0400

    Add files via upload

---
+++ Python/subset.py
@@ -0,0 +1,60 @@
+"""
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+"""
+# Solution 1 - cascading
+# Time and space complexity of of O(N X 2^N)
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        output = [[]]
+
+        for num in nums:
+            output += [curr + [num] for curr in output]
+
+        return output
+
+#Solution 2 - Backtracking
+# Time complexity: O(N X 2^N)
+#Space complexity: O(N)
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
