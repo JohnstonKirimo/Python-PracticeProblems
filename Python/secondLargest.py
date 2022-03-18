8adda62357956d1e92cb3e975860c343981d5582
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-03-18 01:40:16 -0400

    Add files via upload

---
+++ Python/secondLargest.py
@@ -0,0 +1,23 @@
+
+n=int(input())
+a=list(map(int, input().split()))
+a=list(set(a))
+a.sort()
+print(a[len(a)-2])
+
+#Alternative - (better solution)
+n=int(input())
+a=map(int,input().strip().split(" "))
+a=list(a)
+mx1=a[0]
+mn=a[0]
+for i in a:
+    if(i>mx1):
+        mx1=i
+    if(i<mn):
+        mn=i
+mx2=mn
+for i in a :
+    if(i>mx2 and i<mx1):
+        mx2=i
+print(mx2)
