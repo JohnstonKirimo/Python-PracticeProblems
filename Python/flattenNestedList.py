7c1febc5e544932b8245b48c25140ebb10ba230f
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-05-08 01:39:41 -0400

    Add files via upload

---
+++ Python/flattenNestedList.py
@@ -0,0 +1,58 @@
+"""
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists. Implement an iterator to flatten it.
+
+Implement the NestedIterator class:
+
+NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+int next() Returns the next integer in the nested list.
+boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+Your code will be tested with the following pseudocode:
+
+initialize iterator with nestedList
+res = []
+while iterator.hasNext()
+    append iterator.next() to the end of res
+return res
+If res matches the expected flattened list, then your code will be judged as correct.
+ 
+
+Example 1:
+
+Input: nestedList = [[1,1],2,[1,1]]
+Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+Example 2:
+
+Input: nestedList = [1,[4,[6]]]
+Output: [1,4,6]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+ 
+
+Constraints:
+
+1 <= nestedList.length <= 500
+The values of the integers in the nested list is in the range [-106, 106].
+
+"""
+
+#sample solution 
+class NestedIterator:
+    
+    def __init__(self, nestedList: [NestedInteger]):
+        def flatten_list(nested_list):
+            for nested_integer in nested_list:
+                if nested_integer.isInteger():
+                    self._integers.append(nested_integer.getInteger())
+                else:
+                    flatten_list(nested_integer.getList()) 
+        self._integers = []
+        self._position = -1 # Pointer to previous returned.
+        flatten_list(nestedList)
+    
+    def next(self) -> int:
+        self._position += 1
+        return self._integers[self._position]
+        
+    def hasNext(self) -> bool:
+        return self._position + 1 < len(self._integers)
+        
