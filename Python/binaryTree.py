7c045b87c7fa21a8cc5ea2806f35774b833ed2db
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-05-28 14:23:50 -0400

    Add files via upload

---
+++ Python/binaryTree.py
@@ -0,0 +1,30 @@
+""" 
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: 1
+Example 2:
+
+
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+
+"""
+# sample solution
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+        return node.val
+        
